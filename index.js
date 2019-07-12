ed3bd786029f4cb876aa72573d03b39d4bc53f04
Alexander Riccio <test35965+learn@gmail.com>
2019-07-12 16:57:41 -0400

    solve lab, that was easy

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(driver) {
+    drivers.push(driver);
+}
+
+function destructivelyPrependDriver(driver) {
+    drivers.unshift(driver);
+
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+
+function appendDriver(driver) {
+    let newArray = drivers.slice();
+    newArray.push(driver);
+    return newArray; 
+}
+
+function prependDriver(driver) {
+    let newArray = drivers.slice();
+    newArray.unshift(driver);
+    return newArray;
+}
+
+function removeLastDriver() {
+    let newArray = drivers.slice();
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstDriver() {
+    let newArray = drivers.slice();
+    newArray.shift();
+    return newArray;
+}
